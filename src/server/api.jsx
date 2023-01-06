6685535d0fa165a4c2f8db0dcd619a547d1ac839
Gustavo <gustavo.prog40@gmail.com>
2023-01-06 13:54:33 -0300

    setting model

--- src/server/api.jsx
+++ src/server/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://grid-box-shadow-j4yc0fj0t-gustavofsprog.vercel.app/',
+  baseURL: 'https://grid-box-shadow-n185v8eot-gustavofsprog.vercel.app/',
   // baseURL: 'http://localhost:5000/',
 })
 
