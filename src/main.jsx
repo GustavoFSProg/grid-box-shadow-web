bdbf498112dce5e72111c27cec2017f19f4192ac
Gustavo <gustavo.prog40@gmail.com>
2023-01-08 14:14:43 -0300

    yarn lock

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Routers from './routes'
+import Routers from './Routers'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Routers />
