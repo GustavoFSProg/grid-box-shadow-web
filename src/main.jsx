01c4e468786d17193028c4dc1952d0f8ba8156ee
Gustavo <gustavo.prog40@gmail.com>
2023-01-08 14:19:40 -0300

    uppercase

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Routers from './routes'
+import Routers from './Routes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Routers />
