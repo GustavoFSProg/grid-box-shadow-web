33ce9c3d9aa737e6230bd26a964922120a4b1924
Gustavo <gustavo.prog40@gmail.com>
2023-01-08 12:53:15 -0300

    font type

--- src/styled-app.jsx
+++ src/styled-app.jsx
@@ -20,7 +20,7 @@ export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   /* font-family: 'Arima' ; */
-  font-family: 'Lucida' ;
+  font-family: Lucida ;
   color: #004080;
  /* background: #ccccff; */
  width: 98.8vw;
