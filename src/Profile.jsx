fcb85363210e6f626bbd48f29ff6fa6de7a37c27
Gustavo <gustavo.prog40@gmail.com>
2023-01-10 09:40:48 -0300

    letter

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,6 +1,24 @@
 import { useEffect, useState } from 'react'
 import api from './server/api'
 import Header from './components/Header/Header'
+import styled from 'styled-components'
+
+export const Button = styled.button`   
+ display: flex;
+ align-items: center ;
+ justify-content: center ;
+ width: 22rem;
+ height: 35px;
+ background: #004d4d;
+ border-radius: 3px;
+ padding-top: 20px;
+ padding-bottom: 20px;
+ margin-top: 30px;
+ color: white;
+ font-size: 14px;
+ letter-spacing: 1px ;
+
+` 
 
 
 function Profile() {
@@ -8,7 +26,6 @@ function Profile() {
 
   const ID = localStorage.getItem('Id')
   async function handleProfile() {
-
     const { data } = await api.get(`/profile/${ID}`)
 
     setProducts(data)
@@ -17,47 +34,75 @@ function Profile() {
   }
 
   useEffect(() => {
-
     handleProfile()
   }, [])
-
 
   return (
     <>
       <Header />
 
-    <div
-      style={{
-        display: 'flex',
-        width: '100vw',
+      <div
+        style={{
+          display: 'flex',
+          width: '100vw',
           height: '100vh',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
       >
+        <br />
 
-      <br />
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'flex-start',
+            justifyContent: 'left',
 
-      <p>
-        <h2>
-          {product.title}
-        </h2>
+          }}
+        >
+          <h2>{product.title}</h2>
 
-      </p>
-      <img src={product.image} alt="imagem" width="350" />
-      <p>
-        <h2>
+          <img src={product.image} alt="imagem" width="450" />
+          <h2>{product.desc}</h2>
 
-          R$ {product.price}
-        </h2>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
 
-          <button>Comprar</button>
 
-      </p>
+            }}
+          >
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                // alignItems: 'center',
+                justifyContent: 'left',
+              }}
+            >
+              <h2>R$ {product.price}</h2>
+              <h4>Quantidade</h4>
+
+              <input type="number" placeholder="1" height="150"
+                style={{
+                  width: "70px", fontSize: "18px",
+                  paddingLeft: '18px',
+                  paddingRight: '10px',
+                  paddingTop: '10px'
+                  ,
+                  paddingBottom: '10px'
+                }} />
+            </div>
+            <Button>Adicionar ao Carrinho</Button>
+
+          </div>
+        </div>
+
       </div>
     </>
-
   )
 }
 
